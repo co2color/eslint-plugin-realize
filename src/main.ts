50d71034551b7209027aed86cc9b39deb1a96939
co2color <819109255@qq.com>
2024-07-15 23:26:57 +0800

    feat: rule-tester

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-const test = {
-  a: 1
-}
+import { RuleTester } from 'eslint'
+
+const ruleTester = new RuleTester()
+
+ruleTester.run('no-foo', rule, {
+  valid: [
+    {
+      code: 'const bar = 1;'
+    }
+  ],
+  invalid: [
+    {
+      code: 'const foo = 1;',
+      errors: [{ message: 'Unexpected foo' }]
+    }
+  ]
+})
